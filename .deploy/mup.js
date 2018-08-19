d1ae600c8e82ddcaee073d853bb46b6d53c3c122
joe1220 <joe1220@daum.net>
2018-08-19 19:18:31 +0900

    fix google change secret key

---
+++ .deploy/mup.js
@@ -0,0 +1,51 @@
+module.exports = {
+  servers: {
+    one: {
+      // TODO: set host address, username, and authentication method
+      host: '54.250.150.39',
+      username: 'ubuntu',
+      pem: './waggle2.pem'
+      // password: 'server-password'
+      // or neither for authenticate from ssh-agent
+    }
+  },
+
+  app: {
+    // TODO: change app name and path
+    name: 'waggleChat',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      // TODO: Change to your app's url
+      // If you are using ssl, it needs to start with https://
+      ROOT_URL: 'http://ec2-54-250-150-39.ap-northeast-1.compute.amazonaws.com',
+      MONGO_URL: 'mongodb://joe1220:c159789c@ds227352.mlab.com:27352/wagglechat2',
+    },
+
+    // ssl: { // (optional)
+    //   // Enables let's encrypt (optional)
+    //   autogenerate: {
+    //     email: 'email.address@domain.com',
+    //     // comma separated list of domains
+    //     domains: 'website.com,www.website.com'
+    //   }
+    // },
+
+    docker: {
+      // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
+      image: 'abernix/meteord:node-8.4.0-base',
+    },
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true
+  }
+};
